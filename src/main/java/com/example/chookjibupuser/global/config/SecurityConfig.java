2d2ea033fecc531d5dec1512338708205c9d43b8
leeedongjaee <harold306122@gmail.com>
2026-08-11 00:03:22 +0900

    feat: cors 설정 추가

--- src/main/java/com/example/chookjibupuser/global/config/SecurityConfig.java
+++ src/main/java/com/example/chookjibupuser/global/config/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "https://user.chookjibup.store"
+
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
